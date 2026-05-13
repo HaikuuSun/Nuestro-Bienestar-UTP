52f1cbb2b14b637a13b6b21d5cd5a8c42e8464b3
Sergio Mejia <sergio12mg25@gmail.com>
2026-05-13 17:32:17 -0500

    Definición de rutas para categorías con reglas de acceso

---
+++ back-end/src/routes/categorias.routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const categoriaController = require('../controllers/categoria.controller');
+const { autenticarJWT, autorizarRol } = require('../middlewares/auth.middleware');
+const ROLES = require('../config/roles');
+
+// Admins: Registran categorías
+router.post('/crear',
+    autenticarJWT,
+    autorizarRol(ROLES.ADMIN),
+    categoriaController.crearCategoria
+);
+
+// Cualquier usuario puede obtener las categorías
+// Por ID
+router.get('/:id',
+    autenticarJWT,
+    categoriaController.obtenerCategoriaPorID
+);
+// Todas
+router.get('/todas',
+    autenticarJWT,
+    categoriaController.obtenerCategorias
+);
+
+// Admins: Actualizan categorías
+router.put('/actualizar/:id',
+    autenticarJWT,
+    autorizarRol(ROLES.ADMIN),
+    categoriaController.actualizarCategoria
+);
+
+// Admins: Eliminan categorías
+router.delete('/eliminar/:id',
+    autenticarJWT,
+    autorizarRol(ROLES.ADMIN),
+    categoriaController.eliminarCategoria
+);
