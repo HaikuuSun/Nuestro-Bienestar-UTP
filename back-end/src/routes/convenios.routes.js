842411c32de041a32ecd25b020366e58236c49c3
Sergio Mejia <sergio12mg25@gmail.com>
2026-05-13 17:40:24 -0500

    Definición de rutas para convenios con reglas de acceso

---
+++ back-end/src/routes/convenios.routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const conveniosController = require('../controllers/convenios.controller');
+const { autenticarJWT, autorizarRol } = require('../middlewares/auth.middleware');
+const ROLES = require('../config/roles');
+
+// Admins y coordinadores registran convenios
+router.post('/crear',
+    autenticarJWT,
+    autorizarRol(ROLES.ADMIN, ROLES.COORDINADOR),
+    conveniosController.crearConvenio
+);
+
+// Cualquier usuario puede ver los convenios
+// Por ID
+router.get('/:id',
+    autenticarJWT,
+    conveniosController.obtenerConvenioPorID
+);
+// Todos
+router.get('/todos',
+    autenticarJWT,
+    conveniosController.obtenerConvenios
+);
+
+// Admins y coordinadores actualizan convenios
+router.put('/actualizar/:id',
+    autenticarJWT,
+    autorizarRol(ROLES.ADMIN, ROLES.COORDINADOR),
+    conveniosController.actualizarConvenio
+);
+
+// Admins y coordinadores eliminan convenios
+router.delete('/eliminar/:id',
+    autenticarJWT,
+    autorizarRol(ROLES.ADMIN, ROLES.COORDINADOR),
+    conveniosController.eliminarConvenio
+);
