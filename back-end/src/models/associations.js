dbc9448d796fa2e090416012f04601392999b488
Sergio Mejia <sergio12mg25@gmail.com>
2026-05-14 13:38:57 -0500

    Se añade el mapeo N:N entre categorías y usuarios para lograr suscripciones

--- back-end/src/models/associations.js
+++ back-end/src/models/associations.js
@@ -5,6 +5,7 @@ const Categoria = require('./categoria.model');
 const Convenio = require('./convenio.model');
 const PostConvenio = require('./post_convenio.model');
 const Notificacion = require('./notificacion.model');
+const SuscripcionCategoria = require('./suscripcion_categoria.model');
 
 // ============== MAPEO DE ENTIDADES ============== //
 
@@ -30,10 +31,25 @@ Convenio.belongsToMany(Post, {
     as: 'posts'
 });
 
-// 4. Notificaciones, Usuarios y Posts (Relación corregida)
+// 4. Notificaciones, Usuarios y Posts
 // Una notificación pertenece a un usuario y a un post
 Notificacion.belongsTo(Usuario, { foreignKey: 'usuario_id', targetKey: 'id' }); // 1 : 1
 Usuario.hasMany(Notificacion, { foreignKey: 'usuario_id', sourceKey: 'id' }); // 1 : N
 
 Notificacion.belongsTo(Post, { foreignKey: 'post_id', targetKey: 'id' }); // 1 : 1
 Post.hasMany(Notificacion, { foreignKey: 'post_id', sourceKey: 'id' }); // 1 : N
+
+// 5. Suscripciones: Usuarios ↔ Categorías (N:N con payload)
+Usuario.belongsToMany(Categoria, {
+    through: SuscripcionCategoria,
+    foreignKey: 'usuario_id',
+    otherKey: 'categoria_id',
+    as: 'categorias_suscritas'
+});
+
+Categoria.belongsToMany(Usuario, {
+    through: SuscripcionCategoria,
+    foreignKey: 'categoria_id',
+    otherKey: 'usuario_id',
+    as: 'usuarios_suscritos'
+});
